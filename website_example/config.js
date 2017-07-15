1be568658001eb62f7d288d54e6623f011abd2fa
Matt <matt@mattprimeonline.com>
2017-07-15 15:19:59 +0200

    Changed to Superioir coin block chain explorer

--- website_example/config.js
+++ website_example/config.js
@@ -12,6 +12,6 @@ var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR", "XMR-GBP"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://chainradar.com/xmr/block/";
+var blockchainExplorer = "http://superiorchain.info/block/";
 
-var transactionExplorer = "http://chainradar.com/xmr/transaction/";
+var transactionExplorer = "http://superiorchain.info/tx/";
